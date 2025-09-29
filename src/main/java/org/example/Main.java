d0c46303d643aa624ffb23ae9438fb703620b761
Deltaklotz <deltaklotz@gmail.com>
2025-09-29 17:31:29 +0200

    added all semibot materials and textures

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,12 +23,18 @@ import com.jme3.system.AppSettings;
 import com.jme3.texture.Texture;
 import com.jme3.util.SkyFactory;
 
+import java.util.ArrayList;
+
 public class Main extends SimpleApplication {
 
     private BulletAppState bulletAppState;
     private CharacterControl player;
     private Vector3f walkDirection = new Vector3f();
     private boolean left, right, forward, backward;
+
+
+    //player mode list
+    private ArrayList<Spatial> playerEntities = new ArrayList<Spatial>();
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -49,9 +55,41 @@ public class Main extends SimpleApplication {
         stateManager.attach(bulletAppState);
 
         //initialize textures
-        Texture semibot_tex = assetManager.loadTexture("textures/semibot_01.png");
+        Texture semibot_1 = assetManager.loadTexture("textures/semibot/semibot_01.png");
+        Texture semibot_2 = assetManager.loadTexture("textures/semibot/semibot_02.png");
+        Texture semibot_3 = assetManager.loadTexture("textures/semibot/semibot_03.png");
+        Texture semibot_4 = assetManager.loadTexture("textures/semibot/semibot_04.png");
+        Texture semibot_5 = assetManager.loadTexture("textures/semibot/semibot_05.png");
+        Texture semibot_6 = assetManager.loadTexture("textures/semibot/semibot_06.png");
+        Texture semibot_7 = assetManager.loadTexture("textures/semibot/semibot_07.png");
+        Texture semibot_8 = assetManager.loadTexture("textures/semibot/semibot_08.png");
+        Texture semibot_9 = assetManager.loadTexture("textures/semibot/semibot_09.png");
+
         Texture ground_tex = assetManager.loadTexture("textures/grass_online.jpg");
         ground_tex.setWrap(Texture.WrapMode.Repeat);
+
+        //create materials
+        Material semiMat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat3 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat4 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat5 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat6 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat7 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat8 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material semiMat9 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        semiMat1.setTexture("ColorMap", semibot_1);
+        semiMat2.setTexture("ColorMap", semibot_2);
+        semiMat3.setTexture("ColorMap", semibot_3);
+        semiMat4.setTexture("ColorMap", semibot_4);
+        semiMat5.setTexture("ColorMap", semibot_5);
+        semiMat6.setTexture("ColorMap", semibot_6);
+        semiMat7.setTexture("ColorMap", semibot_7);
+        semiMat8.setTexture("ColorMap", semibot_8);
+        semiMat9.setTexture("ColorMap", semibot_9);
+
+        Material groundMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        groundMat.setTexture("ColorMap", ground_tex);
 
         //rootNode.attachChild(SkyFactory.createSky(getAssetManager(), "textures/sky/sky_25_2k.png", SkyFactory.EnvMapType.EquirectMap));
         viewPort.setBackgroundColor(ColorRGBA.fromRGBA255(64, 223, 255, 255));
@@ -60,8 +98,6 @@ public class Main extends SimpleApplication {
         Box groundBox = new Box(50, 0.1f, 50);
         groundBox.scaleTextureCoordinates(new Vector2f(10, 10));
         Geometry ground = new Geometry("Ground", groundBox);
-        Material groundMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        groundMat.setTexture("ColorMap", ground_tex);
         ground.setMaterial(groundMat);
         ground.setLocalTranslation(0, -0.1f, 0);
         RigidBodyControl groundPhys = new RigidBodyControl(0.0f); // static
@@ -71,20 +107,15 @@ public class Main extends SimpleApplication {
 
         // Load model
         Spatial model = assetManager.loadModel("models/semibot.obj");
-
         CapsuleCollisionShape modelcoll = new CapsuleCollisionShape(1f,2f);
-
         RigidBodyControl modelcontroll = new RigidBodyControl(modelcoll, 0);
         model.addControl(modelcontroll);
         bulletAppState.getPhysicsSpace().add(modelcontroll);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setTexture("ColorMap", semibot_tex);
-        mat.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Off);
-        model.setMaterial(mat);
+        model.setMaterial(semiMat1);
         model.getControl(RigidBodyControl.class).setPhysicsLocation(new Vector3f(0,2,5));
         model.setLocalScale(1.25f);
         rootNode.attachChild(model);
+
 
         // Character (first-person)
         CapsuleCollisionShape capsule = new CapsuleCollisionShape(1f, 2f);
@@ -137,7 +168,7 @@ public class Main extends SimpleApplication {
 
         walkDirection.y = 0; // keep horizontal
         player.setWalkDirection(walkDirection.mult(0.25f));
-        player.setJumpSpeed(10f); // lower jump
+        player.setJumpSpeed(12f); // lower jump
         player.setFallSpeed(30f);
         player.setGravity(30f);
         cam.setLocation(player.getPhysicsLocation().add(0, 1.5f, 0));
